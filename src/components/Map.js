39fdb2e338b68b27b0d861b907ceeaa328af54c0
Sajantoor98 <Sajantoor98@gmail.com>
2020-03-27 22:51:04 -0700

    :sparkles: Added Text labels, color pickin algorithm and created deck file

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,75 +1,10 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
+import { scatterPlotLayer, heatMapLayer, textLayer } from './Deck';
 import { GoogleMapsOverlay } from '@deck.gl/google-maps';
-import { ScatterplotLayer, GeoJsonLayer } from '@deck.gl/layers';
-import { HeatmapLayer } from '@deck.gl/aggregation-layers';
-import data from '../data.json';
-import geoJson from '../geo.json';
 import mapStyles from './map-styles';
 
 const GOOGLE_MAP_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-
-const scatterPlotLayer = () => new ScatterplotLayer({
-  id: 'scatter',
-  data: data.locations,
-  opacity: 0.8,
-  filled: true,
-  radiusMaxPixels: 5,
-  radiusMinPixels: 3,
-  getPosition: d => [parseInt(d.coordinates.longitude), parseInt(d.coordinates.latitude)],
-  getFillColor: d => d.latest.confirmed > 10 ? [200, 0, 40, 150] : [255, 140, 0, 100],
-});
-
-const heatMapLayer = () => new HeatmapLayer({
-  id: 'heat',
-  data: data.locations,
-  getPosition: d => [parseInt(d.coordinates.longitude), parseInt(d.coordinates.latitude)],
-  getWeight: d => parseInt(d.latest.confirmed),
-  radiusPixels: 50,
-  threshold: 0.005,
-});
-
-const geoJsonLayer = () => new GeoJsonLayer({
-  id: 'geo-json',
-  data: geoJson,
-  stroked: false,
-  filled: true,
-  extruded: true,
-  lineWidthScale: 20,
-  lineWidthMinPixels: 2,
-  getFillColor: d => getWeight(d.properties.name),
-  getRadius: 100,
-})
-
-const leader = data.locations.find(element => element.country === "US").latest.confirmed;
-
-function getColor(value) {
-  const low = [255, 158, 158, 255];
-  const high = [163, 28, 28, 255];
-
-  // delta represents where the value sits between the min and max
-  let delta = value / leader;
-
-  let color = [];
-  for (var i = 0; i < 3; i++) {
-    color[i] = (high[i] - low[i]) * delta + low[i];
-  }
-
-  return high;
-
-}
-
-function getWeight(name) {
-  try {
-    let obj = data.locations.find(element => element.province === name || element.country === name);
-    let weight = obj.latest.confirmed;
-    let color = getColor(weight);
-    return color;
-  } catch (error) {
-    return 0;
-  }
-}
-
 
 class GoogleMap extends React.Component {
   constructor(props) {
@@ -108,9 +43,9 @@ class GoogleMap extends React.Component {
   initLayers() {
     const overlays = new GoogleMapsOverlay({
       layers: [
-        // geoJsonLayer(),
         scatterPlotLayer(),
-        heatMapLayer(),
+        textLayer(),
+        // heatMapLayer(),
       ]
     });
 
