e390d7596c0702677ba03ab63f50496f68f97b84
Sajantoor98 <Sajantoor98@gmail.com>
2020-05-04 19:28:46 -0700

    :warning: Figured out data but Google isn't defined error in init layers method

--- src/App.js
+++ src/App.js
@@ -23,5 +23,47 @@ function convertToLocalTime(value) {
   return date.toLocaleString();
 }
 
-export { fetchData, convertToLocalTime };
+
+async function getData() {
+  const data = {
+    latest: {},
+    locations: [],
+  };
+  
+  // sources
+  const COVID19API_WORLD = "https://coronavirus-tracker-api.herokuapp.com/v2/locations?timelines=true";
+  const COVID19API_US = "https://coronavirus-tracker-api.herokuapp.com/v2/locations?source=csbs&timelines=true";
+  // fetch world data and push to data object
+  fetchData(COVID19API_WORLD).then(worldData => {
+    // pushes to main data object
+    for (var i = 0; i < worldData.locations.length; i++) {
+      let obj = worldData.locations[i];
+      obj.coordinates.latitude = parseFloat(obj.coordinates.latitude);
+      obj.coordinates.longitude = parseFloat(obj.coordinates.longitude);
+      // removes all US data as US has it's own source and removes NULL island
+      if (obj.country !== "US" && obj.province !== "Grand Princess" && !(obj.coordinates.latitude === 0 && obj.coordinates.longitude === 0)) {
+        obj.last_updated = convertToLocalTime(obj.last_updated);
+        data.locations.push(obj);
+      }
+    }
+
+    data.latest = worldData.latest;
+  });
+  // fetches us data and push to data object
+  fetchData(COVID19API_US).then(USData => {
+    for (var i = 0; i < USData.locations.length; i++) {
+      USData.locations[i].last_updated = convertToLocalTime(USData.locations[i].last_updated);
+      USData.locations[i].coordinates.latitude = parseFloat(USData.locations[i].coordinates.latitude);
+      USData.locations[i].coordinates.longitude = parseFloat(USData.locations[i].coordinates.longitude);
+      data.locations.push(USData.locations[i]);
+    }
+  });
+
+  return data;
+}
+
+getData();
+
+
+export { getData, convertToLocalTime };
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -4,16 +4,10 @@ import { GoogleMapsOverlay } from '@deck.gl/google-maps';
 import mapStyles from './map-styles';
 // eslint-disable-next-line
 import styles from './css/map.css';
-import { fetchData, convertToLocalTime } from '../App';
+import { getData, convertToLocalTime } from '../App';
 import Loading from './Loading';
 import { getGoogleAPI } from '../Firebase';
 import { ReactComponent as GPS } from '../assets/icons/gps.svg';
-
-// data from multiple sources
-let data = {
-  latest: {},
-  locations: [],
-};
 
 class GoogleMap extends React.Component {
   constructor(props) {
@@ -23,8 +17,8 @@ class GoogleMap extends React.Component {
 
     this.state = {
       layers: [], // which layers are being used
-      worldData: false, // check if world data is fetched
-      usData: false, // check if us data is fetched
+      data: false,
+      googleInit: false,
       dataParameter: "confirmed", // which dataParameter is currently displayed, confirmed, deaths or recovered
       heatMap: false, // layer booleans
       scatterPlot: true,
@@ -33,19 +27,27 @@ class GoogleMap extends React.Component {
 
   googleMapRef = React.createRef();
 
-  componentDidMount() {
+  componentWillMount() {
+    getData().then((value) => {
+      this.setState({data: value});
+    });
     // create the google map component and loads the script
     const googleMapScript = document.createElement('script');
     // gets the Google Maps API Key from firebase and creates the map
+    const this_ = this;
+
     getGoogleAPI().then(function(value) {
+      console.log(value.data);
       const GOOGLE_MAP_API_KEY = value.data;
       googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=visualization`;
-    });
 
-    window.document.body.appendChild(googleMapScript);
-    // initializes the map
-    googleMapScript.addEventListener('load', () => {
-      this.googleMap = this.createGoogleMap();
+      window.document.body.appendChild(googleMapScript);
+      // initializes the map
+      googleMapScript.addEventListener('load', () => {
+        this_.googleMap = this_.createGoogleMap();
+      });
+
+      this_.setState({googleInit: true});
     });
   }
 
@@ -57,6 +59,14 @@ class GoogleMap extends React.Component {
       center: {
         lat:  30,
         lng:  0,
+      },
+      restriction: {
+        latLngBounds: {
+            north: 85,
+            south: -85,
+            west: -180,
+            east: 180
+        }
       },
       disableDefaultUI: true,
       styles: mapStyles,
@@ -81,41 +91,6 @@ class GoogleMap extends React.Component {
     this.googleMap.setZoom(7);
   }
 
-  // fetches the data from the apis
-  getData() {
-    // sources
-    const COVID19API_WORLD = "https://coronavirus-tracker-api.herokuapp.com/v2/locations";
-    const COVID19API_US = "https://coronavirus-tracker-api.herokuapp.com/v2/locations?source=csbs";
-    // fetch world data and push to data object
-    fetchData(COVID19API_WORLD).then(worldData => {
-      // pushes to main data object
-      for (var i = 0; i < worldData.locations.length; i++) {
-        let obj = worldData.locations[i];
-        obj.coordinates.latitude = parseFloat(obj.coordinates.latitude);
-        obj.coordinates.longitude = parseFloat(obj.coordinates.longitude);
-        // removes all US data as US has it's own source and removes NULL island
-        if (obj.country !== "US" && obj.province !== "Grand Princess" && !(obj.coordinates.latitude === 0 && obj.coordinates.longitude === 0)) {
-          obj.last_updated = convertToLocalTime(obj.last_updated);
-          data.locations.push(obj);
-        }
-      }
-
-      data.latest = worldData.latest;
-      this.setState({worldData: true});
-    });
-    // fetches us data and push to data object
-    fetchData(COVID19API_US).then(USData => {
-      for (var i = 0; i < USData.locations.length; i++) {
-        USData.locations[i].last_updated = convertToLocalTime(USData.locations[i].last_updated);
-        USData.locations[i].coordinates.latitude = parseFloat(USData.locations[i].coordinates.latitude);
-        USData.locations[i].coordinates.longitude = parseFloat(USData.locations[i].coordinates.longitude);
-        data.locations.push(USData.locations[i]);
-      }
-
-      this.setState({usData: true});
-    });
-  }
-
   // resize the map on window's resize
   resize() {
     let currCenter = this.googleMap.getCenter();
@@ -126,9 +101,9 @@ class GoogleMap extends React.Component {
   // initalizes the deck gl layers based off the data and the dataParameter
   initLayers() {
     const layers = [
-      this.state.heatMap ? heatMapLayer(data, this.state.dataParameter) : null ,
-      this.state.scatterPlot ? scatterPlotLayer(data, this.state.dataParameter) : null ,
-      hoverPlotLayer(data, this.state.dataParameter),
+      this.state.heatMap ? heatMapLayer(this.state.data, this.state.dataParameter) : null ,
+      this.state.scatterPlot ? scatterPlotLayer(this.state.data, this.state.dataParameter) : null ,
+      hoverPlotLayer(this.state.data, this.state.dataParameter),
     ];
     // sets overlay to google map
     this.overlay = new GoogleMapsOverlay({
@@ -141,11 +116,11 @@ class GoogleMap extends React.Component {
 
   // used to change layers based off the layer booleans
   changeLayers() {
-    getInfo(data, this.state.dataParameter);
+    getInfo(this.state.data, this.state.dataParameter);
     const layers = [
-      this.state.heatMap ? heatMapLayer(data, this.state.dataParameter) : null,
-      this.state.scatterPlot ? scatterPlotLayer(data, this.state.dataParameter) : null,
-      hoverPlotLayer(data, this.state.dataParameter),
+      this.state.heatMap ? heatMapLayer(this.state.data, this.state.dataParameter) : null,
+      this.state.scatterPlot ? scatterPlotLayer(this.state.data, this.state.dataParameter) : null,
+      hoverPlotLayer(this.state.data, this.state.dataParameter),
     ]
 
     this.setState({layers: layers}); // updates the state
@@ -161,9 +136,10 @@ class GoogleMap extends React.Component {
     // ignores unwanted things to avoid max depth limit
     state.layers = 0;
     prevStateObj.layers = 0;
+    
     // init layers is both data sources have been fetched, ignores this if layers have been init
-    if (this.state.usData && this.state.worldData && stateLayers === 0) {
-      getInfo(data, this.state.dataParameter);
+    if (this.state.data && this.state.googleInit && stateLayers === 0) {
+      getInfo(this.state.data, this.state.dataParameter);
       this.initLayers();
     // checks if anything has been updates and if layers have been init
     } else if ((JSON.stringify(state) !== JSON.stringify(prevStateObj)) && stateLayers !== 0) {
@@ -171,9 +147,6 @@ class GoogleMap extends React.Component {
     }
   }
 
-  componentWillMount() {
-    this.getData();
-  }
 
   render() {
     return (
