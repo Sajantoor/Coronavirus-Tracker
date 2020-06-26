e02bb46a01661779e5ba400b0a41a851ea417302
Sajantoor98 <Sajantoor98@gmail.com>
2020-06-25 18:02:01 -0700

    Updates

---
+++ precache-manifest.cb8eb09fa31af1fac60b97f3d2202abc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868afb429ecd722c86302f3d38ffcda8",
+    "url": "/Coronavirus-Tracker/index.html"
+  },
+  {
+    "revision": "cf40918d66e38e5ba272",
+    "url": "/Coronavirus-Tracker/static/css/main.56619a2d.chunk.css"
+  },
+  {
+    "revision": "b0fe397f3244f04abea9",
+    "url": "/Coronavirus-Tracker/static/js/2.a210320c.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/Coronavirus-Tracker/static/js/2.a210320c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf40918d66e38e5ba272",
+    "url": "/Coronavirus-Tracker/static/js/main.b128d48f.chunk.js"
+  },
+  {
+    "revision": "b649b932d976f870c01f",
+    "url": "/Coronavirus-Tracker/static/js/runtime-main.bd87175e.js"
+  },
+  {
+    "revision": "e8ba5c155c18db198fee863fd5539783",
+    "url": "/Coronavirus-Tracker/static/media/gps.e8ba5c15.svg"
+  }
+]);
